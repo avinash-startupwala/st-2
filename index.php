fd0499c157364cfaed7f5753c7d1d99bfd851722
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2018-04-23 19:37:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
   
   
   
-  <table align="center" bgcolor="#F0F0F0" border="0" cellpadding="0" cellspacing="0" width="800">
+  <table align="center" bgcolor="#F0F0F0" border="0" cellpadding="0" cellspacing="0" width="800px">
 	<tbody>
 		<tr>
 			<td align="center">
