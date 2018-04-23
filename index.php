c8560a862c44347a582eb19a8ec131147a7a806d
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2018-04-23 19:25:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,82 @@
-<?php
-echo " Hello world ";
+<html>
+ <head>
+ <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
 
-?>
+<style>
+
+  
+  body{
+
+  font-family: Lato;
+
+}
+
+
+.center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+
+    width: 100%;
+    height : 70px;
+  
+}
+
+.button {
+
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    width: 17%;
+
+    
+}
+.link {
+  text-align: center;
+}
+</style>
+  <img src="https://www.startupwala.com/images/logo_startupwala_new.png"  width="100%"  />
+  <!-- Begin Default Content REMOVE THIS -->
+  <h1 align="center" style="color:#3293EA; "><font size="6"><b>Thank You!</b></font></h1>
+
+  <p align="center" > <font size="4"> for submitting your details, <br />
+  we will call you back in an hour <br /> <br /> ***  <br /> <br /> <a style="color:#46A3EB;" href="https://www.startupwala.com/" target="_blank">
+  Startupwala.com </a>  is <br /> India&apos;s #1 Online Legal Services Platform <br /> for Startups &amp; SMEs.</font> 
+  </p>
+  
+  
+  
+  
+  <a   href="https://bit.ly/StartupwalaApp" target="_blank"><apex:image id="yy" value="https://www.startupwala.com/images/google_store.jpg"  styleClass="button" /></a>
+
+  
+  <p align="center"  > &amp; GET </p>
+  
+    <div class="link">
+    <a style="color:#46A3EB;" href="https://www.startupwala.com/free-21-legal-agreements-for-startups/" target="_blank"><font size="4.7"> 21 Must Have Startup Legal Agreements for Free </font>  </a>
+     </div>
+    
+     <p  align="center" ><font size="4.7"> These Free Do-It-Yourself Agreements are  </font> </p>
+     
+     
+     <p align="center" ><i>Handcrafted by Legal Experts <span style="color: #2196f3;"> | </span> Worth more than Rs. 70,000 <span style="color: #2196f3;"> | </span> Used by 75,000 Startups </i> </p>
+     
+     
+     <p align="center" style="font-family:Lato; font-size:13px; color:#999999;font-weight: normal;">
+            © 2017 STARTUPWALA
+     <br />  
+     Served 150,000+ Startups | 9.6 Trust Score | 10+ Years Expertise | 100+ Team <br />
+     Pune I Bangalore I Mumbai I Chennai I Delhi
+     </p>
+     
+     <p align="center" style="font-family:Century gothic, Arial, Sans-serif; font-size:13px; color:#999999;font-weight: normal;">
+        </p>
+     
+     
+  <!-- End Default Content REMOVE THIS -->
+  
+  </html> 
+                   
